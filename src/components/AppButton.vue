bea5e1826970d166be8dafb296458cd1f0d43589
Miłosz Mandowski <milosz.mandowski@owlsdepartment.com>
2020-03-18 12:09:38 +0100

    Init

---
+++ src/components/AppButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button type="button" @click="onClick">
+    {{ innerText }}
+  </button>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    innerText: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.$emit("onClick");
+    }
+  }
+});
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <hello-world msg="Welcome to Your Vue.js App" />
+
+    <app-button />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import AppButton from "@/components/AppButton.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    AppButton
   }
 };
 </script>
